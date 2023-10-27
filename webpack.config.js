aeef793427565cd1135c202a1a5d48a00a3ed43e
Chris Rowles <c.rowles@sentrybay.com>
2023-10-27 18:01:18 +0100

    0.3.0

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: [
     './src/index.js',
   ],

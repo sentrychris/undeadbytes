564c2d0ce3d0e2b9e822b8a4fc94ce6791fa5122
sfazli96 <sfazli96@gmail.com>
2023-10-30 14:16:11 -0700

    level-design, level 7

---
+++ src/browser/lib/Levels/level7.js
@@ -0,0 +1,35 @@
+/**
+ * Level 7
+ *
+ * 26 Enemies
+ * 14 Ammo
+ * 07 Health
+ * 07 Stamina
+ */
+export const Level7 = [
+  'W W W W W W W W W W W W W W W W W W W W W W W W W W',
+  'W               W                 W   W   W   W   W',
+  'W   A A   P     W   E   W   E     W W W   H   A   W',
+  'W               W   A   W   A   A   W   A   W   A W',
+  'W W W W W W W W W   S   W   S   S W W W   A   S   W',
+  'W               W   A   W   A   A   W   E   W   E W',
+  'W       S       W       W W       W   A   W       W',
+  'W   E     E     W       W         W   H   W       W',
+  'W               W     E     E       W   E   W     W',
+  'W       W       W     S   H   S   H   A   W       W',
+  'W   A   W W     W W W W W W W W   W   W   W       W',
+  'W                                       W         W',
+  'W                   H     S         S   W   E     W',
+  'W   H   W W     W   A     E       A     W   H     W',
+  'W       W       W     H           H   S W     S   W',
+  'W               W     W W       W W W W   W   A   W',
+  'W   E     E     W   H     H     H       W         W',
+  'W     S         W   S     A     S     A W   H   S W',
+  'W               W     W W   S   W W W W   W       W',
+  'W W W W W W W W W             W             W   E W',
+  'W               W   E     E   W       W     E     W',
+  'W   A   E       W             W   E   W           W',
+  'W   H   E     E   E     E     H   E   W   A H     W',
+  'W               W             W                   W',
+  'W W W W W W W W W W W W W W W W W W W W W W W W W W'
+  ];

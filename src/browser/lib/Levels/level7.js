74565fc319d46d1d294a708871d76b2f980973ad
sfazli96 <sfazli96@gmail.com>
2023-10-30 15:18:52 -0700

    modified level7 changes

--- src/browser/lib/Levels/level7.js
+++ src/browser/lib/Levels/level7.js
@@ -3,33 +3,33 @@
  *
  * 26 Enemies
  * 14 Ammo
- * 07 Health
- * 07 Stamina
+ * 08 Health
+ * 08 Stamina
  */
 export const Level7 = [
   'W W W W W W W W W W W W W W W W W W W W W W W W W W',
   'W               W                 W   W   W   W   W',
-  'W   A A   P     W   E   W   E     W W W   H   A   W',
-  'W               W   A   W   A   A   W   A   W   A W',
-  'W W W W W W W W W   S   W   S   S W W W   A   S   W',
-  'W               W   A   W   A   A   W   E   W   E W',
+  'W     A   P     W   E   W   E     W W W   H   A   W',
+  'W               W   A   W       A   W   A   W   A W',
+  'W         W W W W   S   W   S     W W W   A   S   W',
+  'W               W       W           W   E   W   E W',
   'W       S       W       W W       W   A   W       W',
   'W   E     E     W       W         W   H   W       W',
   'W               W     E     E       W   E   W     W',
-  'W       W       W     S   H   S   H   A   W       W',
+  'W       W       W         H       H   A   W       W',
   'W   A   W W     W W W W W W W W   W   W   W       W',
   'W                                       W         W',
-  'W                   H     S         S   W   E     W',
-  'W   H   W W     W   A     E       A     W   H     W',
-  'W       W       W     H           H   S W     S   W',
-  'W               W     W W       W W W W   W   A   W',
-  'W   E     E     W   H     H     H       W         W',
-  'W     S         W   S     A     S     A W   H   S W',
+  'W                   H                   W   E     W',
+  'W   H   W W     W   A     E       A     W         W',
+  'W       W       W                       W         W',
+  'W               W     W W       W W W W   W       W',
+  'W   E     E     W                       W         W',
+  'W     S         W   S           S     A W   H     W',
   'W               W     W W   S   W W W W   W       W',
   'W W W W W W W W W             W             W   E W',
   'W               W   E     E   W       W     E     W',
   'W   A   E       W             W   E   W           W',
-  'W   H   E     E   E     E     H   E   W   A H     W',
-  'W               W             W                   W',
+  'W       E     E   E     E     H   E   W   A       W',
+  'W     H         W             W                   W',
   'W W W W W W W W W W W W W W W W W W W W W W W W W W'
   ];

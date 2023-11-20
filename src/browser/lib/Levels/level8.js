310b66813ca86d8e86bb32e638f35a7f72fe6436
CrisitoJ <cjchaverri@uned.cr>
2023-11-20 18:51:03 +0000

    added level 8

---
+++ src/browser/lib/Levels/level8.js
@@ -0,0 +1,47 @@
+/**
+ * Level 7
+ *
+ * 22 Enemies
+ * 12 Ammo
+ * 04 Health
+ * 03 Stamina
+ * @category Game Scene
+ * @subcategory Level
+ * @memberof Level
+ * @module level7
+ */
+export const Level7 = [
+  'W W W W W W W W W W W W W W W W W W W W W W W W W W',
+		'W																																																	W',
+		'W  E  E													  													S							 						W',
+		'W																																																	W',
+		'W														W																			W							E						W',
+		'W																																																	W',
+		'W														W																			W	W												W',
+		'W																																																	W',
+		'W														W																					W					S						W',
+		'W																															E		H 													W',
+		'W				E						H		W																					W												W',
+		'W																															E																	W',
+		'W		A	 A	 A		A		W																					W					E						W',
+		'W																				S																												W',
+		'W W	W W W W W	 W													W	W	W	W	W			  							W',
+		'W																																																	W',
+		'W	 E  E  E		E		W													W		E		W						E							W',
+		'W																																																	W',
+		'W					A		A					W													W		E		W												  W',
+		'W																																																	W',
+		'W		H		E  E  E		W													W		E		W														W',
+		'W																																																	W',
+		'W					E		A					W													W														A		A	 W',
+		'W																																																	W',
+		'W														W												 W	W	W W	W W W W W				W',
+		'W																																																	W',
+		'W																										 W		A		E  E  E  E			A		W',
+		'W																																																	W',
+  'W W W W W W W W W W W W     W W W W W W W W W W W W',
+		'W																						  																									W',
+		'W		H																				P																						A		W',
+		'W																																																	W',
+  'W W W W W W W W W W W W W W W W W W W W W W W W W W',
+];
